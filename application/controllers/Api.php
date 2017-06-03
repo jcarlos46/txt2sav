4bf6383d9640199a9cd8637fcbeb5a9bf33ef8c5
João Carlos <joaocarlos422@gmail.com>
2017-06-03 17:57:25 -0300

    Readicionando id da ocorrencia no retorno da API

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -122,7 +122,6 @@ class Api extends CI_Controller
     {
         $content_final = $this->content_model->getLastByWhere("md5 = '{$md5}'");
         unset($content_final->password);
-        $content_final->id = null;
         $content_final->id_parent = null;
         $this->json($content_final);
     } 
