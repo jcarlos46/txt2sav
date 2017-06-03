9a5c8526cdbf5ef5ac8ef67740de328fc1f348f5
João Carlos <joaocarlos422@gmail.com>
2017-06-03 19:53:44 -0300

    Corrigindo menu para fork de versões anteriores

--- application/views/head.php
+++ application/views/head.php
@@ -1,7 +1,7 @@
 <nav>
     <ul class="menu">
         <li><a href="/">[txt2sav]</a></li>
-        <li><a href="/fork/<?php echo $md5 ?>">[fork]</a></li>
+        <li><a href="/fork/<?php echo $md5 ?>/<?php echo $create_at ?>">[fork]</a></li>
         <li><a href="#">[log]</a></li>
         <li><a href="/edit/<?php echo $md5 ?>">[edit]</a></li>
     </ul>
